17e615c9c59e43f353f4848698295602392101fa
Marin <marinmema@live.ca>
2025-02-04 08:49:50 -0500

    sanitize name

--- src/components/NamePage.tsx
+++ src/components/NamePage.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import { GoToPage } from "../types/GoToPage";
 
 type PropType = {
-	goToPage: GoToPage
+    goToPage: GoToPage;
 };
 
 export default function NamePage({}: PropType) {
-	return (
-		<div className="page">
-			What's your name?
-			<input type="text" />
-			<button type="button">Done</button>
-		</div>
-	)
+    const [name, setName] = useState<string>("");
+
+    function setSanitizedName(input: string): void {
+        const validChars =
+            "abcdefghijklmnopqrstuvwxyz" +
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "0123456789";
+        const sanitized = input
+            .split("")
+            .filter((c) => validChars.includes(c))
+            .join("");
+        setName(sanitized);
+    }
+
+    return (
+        <div className="page">
+            What's your name?
+            <input
+                type="text"
+                value={name}
+                onChange={(event) => setSanitizedName(event.target.value)}
+            />
+            <button type="button">Done</button>
+        </div>
+    );
 }
