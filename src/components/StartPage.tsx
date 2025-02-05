a4ce4547a9166a6a78609c0d46086a4555f082d6
Marin <marinmema@live.ca>
2025-02-04 20:12:32 -0500

    show name bar

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -12,10 +12,12 @@ export default function StartPage({goToPage}: PropType) {
 	return (
 		<div className="page">
 			<h1 className="inverted">Connect Four</h1>
-			Playing as {name} id {id}
-			<button onClick={ () => goToPage("connect") }>New Game</button>
-			<button onClick={ () => goToPage("connect") }>Play</button>
-			<button onClick={ () => goToPage("name") }>Change Name</button>
+			<div className="user-name-bar">
+				{name}
+				<button onClick={ () => goToPage("name") }>Change Name</button>
+			</div>
+			
+			
 		</div>
 	);
 }
