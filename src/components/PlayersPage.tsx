e009e9037848717f84562a5a7d15290603e32202
Marin <marinmema@live.ca>
2025-02-07 07:59:02 -0500

    merge OnlineList back into PlayersPage

--- src/components/PlayersPage.tsx
+++ src/components/PlayersPage.tsx
@@ -1,6 +1,8 @@
-import { GoToPage } from "../types/GoToPage";
-import OnlineList from "./OnlineList";
+import { useState, useEffect } from "react";
 import { Player } from "../types/Player";
+import PlayerListItem from "./PlayerListItem";
+import createPresenceConnection from "../logic/createPresenceConnection";
+import { GoToPage } from "../types/GoToPage";
 
 type Props = {
     myself: Player;
@@ -8,10 +10,42 @@ type Props = {
 };
 
 export default function PlayersPage(props: Props) {
+    const [players, setPlayers] = useState<Player[]>([]);
+
+    useEffect(() => {
+        const connection = createPresenceConnection({
+            myself: props.myself,
+            onChange(players: Player[]) {
+                setPlayers([...players]);
+            },
+        });
+
+        return () => {
+            connection.disconnect();
+        };
+    }, []);
+
+    const others = players.filter((p) => p.id != props.myself.id);
+
     return (
         <div className="page">
             <h1 className="inverted">Connect Four</h1>
-            <OnlineList myself={props.myself} goToPage={props.goToPage} />
+            <div className="online-list">
+                {props.myself.name}{" "}
+                <button onClick={() => props.goToPage("name")}>
+                    Change Name
+                </button>
+                <ul>
+                    {others.map((p) => (
+                        <PlayerListItem
+                            key={p.id}
+                            player={p}
+                            isMyself={p.id === props.myself.id}
+                            goToPage={props.goToPage}
+                        />
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
