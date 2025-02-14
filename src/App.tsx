214949dc86ca7a49a686fb254e60dcfca6f027e5
Marin <marinmema@live.ca>
2025-02-14 15:37:35 -0500

    start over with a blank app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { useEffect, useState } from "react";
-import PresenceConnection from "./logic/PresenceConnection";
-import { Player, PlayerStatus } from "./types/Player";
 import "./App.css";
 
 function App() {
-    const myself: Player = {
-        id: "id",
-        name: "name",
-        status: PlayerStatus.Ready,
-    };
-
-    useEffect(() => {
-        const presenceConnection = new PresenceConnection({
-            myself,
-            onChange: (newPlayers: Player[]) => {},
-        });
-        return () => presenceConnection.disconnect();
-    }, []);
-
-    // connection callbacks need access to state inside component
-    // connection onopen should happen inside component
-
-    return <div></div>;
+    return <div>👾</div>;
 }
 
 export default App;
