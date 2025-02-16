07ca4537ec997945a73b33e7c6f7ac56a4eede7e
Marin <marinmema@live.ca>
2025-02-16 11:03:21 -0500

    delete goToPage

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,14 +1,7 @@
-import { GoToPage } from "../types/GoToPage";
-
-type PropType = {
-    goToPage: GoToPage;
-};
-
-export default function ErrorPage({ goToPage }: PropType) {
+export default function ErrorPage() {
     return (
         <div className="page">
-            <h1>Error Page</h1>
-            <button onClick={() => goToPage("players")}>Back</button>
+            <h1>Error</h1>
         </div>
     );
 }
--- src/components/PlayPage.tsx
+++ src/components/PlayPage.tsx
@@ -1,15 +1,9 @@
-import { GoToPage } from "../types/GoToPage";
 import GameGrid from "./GameGrid";
 
-type PropType = {
-    goToPage: GoToPage;
-};
-
-export default function PlayPage({ goToPage }: PropType) {
+export default function PlayPage() {
     return (
         <div className="page">
             <GameGrid />
-            <button onClick={() => goToPage("players")}>Leave</button>
         </div>
     );
 }
