5b3a26b47cb127a5844e9a5252a44c2b9a78c224
Marin <marinmema@live.ca>
2025-02-17 18:49:55 -0500

    delete Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
 import "./App.css";
-import { Pages } from "./components/Pages";
-import { RoomContext } from "./contexts/RoomContext";
 import { useRoom } from "./hooks/useRoom";
+
+function invite(name: string) {
+    return `invite ${name}`;
+}
 
 function App() {
     const room = useRoom();
+    const myself = "my-name";
+    const players: string[] = [];
+
+    if (room === null) {
+        return "";
+    }
+
+    if (!room.socket.isOnline) {
+        return "No internet connection";
+    }
+
+    if (room.socket.readyState != WebSocket.OPEN) {
+        return (
+            <div className="page">
+                <button onClick={() => room?.join(myself)}>Connect</button>
+            </div>
+        );
+    }
+
     return (
-        <RoomContext.Provider value={room}>
-            <Pages />
-        </RoomContext.Provider>
+        <div className="page">
+            <h1 className="inverted">Connect Four</h1>
+            <div className="online-list">
+                {myself} (myself)
+                <ul>
+                    {players.map((player) => (
+                        <li onClick={() => invite(player)} key={player}>
+                            <div>{player}</div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
+
+    // return (
+    //     <div className="page">
+    //         <GameGrid />
+    //     </div>
+    // );
 }
 
 export default App;
